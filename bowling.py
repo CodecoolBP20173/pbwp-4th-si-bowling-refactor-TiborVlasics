5d9e90695d34fc55a566de4475d32033d4ccc373
Tibor Vlasics <tibor.vlasics@gmail.com>
2017-11-23 18:03:29 +0100

    Complexity removed

--- bowling.py
+++ bowling.py
@@ -7,7 +7,6 @@ def score(game):
             result += 10 - get_score_for_roll(game[roll - 1])
         else:
             result += get_score_for_roll(game[roll])
-
         if frame < 10 and game[roll] in '/Xx':
             result += get_score_for_roll(game[roll+1])
             if game[roll].lower() == 'x':
@@ -15,7 +14,6 @@ def score(game):
                     result += 10 - get_score_for_roll(game[roll+1])
                 else:
                     result += get_score_for_roll(game[roll+2])
-
         if not in_first_half_of_frame or game[roll].lower() == 'x':
             frame += 1
             in_first_half_of_frame = True
